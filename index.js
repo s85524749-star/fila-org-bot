909bbd47a4ea1b0a034f92801dfc2d7b4487a64c
s85524749-star <s85524749@gmail.com>
2026-07-26 07:56:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers
-  ]
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages
+  ],
+  partials: [Partials.Channel]
 });
 
 client.once("ready", () => {
-  console.log(`✅ ${client.user.tag} está online!`);
+  console.log(`✅ ${client.user.tag} iniciado com sucesso!`);
+  console.log("🚀 Bot de Fila Online");
 });
 
-client.login("process.env.TOKEN");
+client.login(process.env.TOKEN);
