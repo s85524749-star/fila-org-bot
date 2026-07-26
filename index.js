6c79a9a0806b103e4c6922c94bca633d5b5528dd
s85524749-star <s85524749@gmail.com>
2026-07-26 07:38:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ ${client.user.tag} está online!`);
+});
+
+client.login("process.env.TOKEN");
